1163ad5e6d9886792c988083fed544911a23c22e
ccarl0 <carlo.cecco0@gmail.com>
2024-05-19 23:09:32 +0200

    server tuning

--- extension/server/server.py
+++ extension/server/server.py
@@ -30,4 +30,4 @@ def download_audio():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80, debug=True)
